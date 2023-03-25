d3023c20819ffa7f05cad8eff240408dbcc91dde
Kyle Boehlen <kyleboehlen@gmail.com>
2023-03-25 00:50:56 -0600

    Day 1 parts 1 and 2

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ import utilities.input as inputReader
 day = input("What day would you like to run? ")
 
 input = inputReader.openInput(day)
+
+match day:
+    case "1":
+        import scripts.day01 as s
+        s.solvePuzzle(input)
